eda1a6cf4d250dbecefb9faebc93e4592777e326
Aspire-1 <873763823@qq.com>
2018-04-26 19:27:59 +0800

    websocket处理器类的修改

--- src/main/java/com/aspire/OnlineMeal/webSocket/MyWebSocketHandler.java
+++ src/main/java/com/aspire/OnlineMeal/webSocket/MyWebSocketHandler.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.CloseStatus;
@@ -14,6 +15,7 @@ import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 import com.aspire.OnlineMeal.publicPOJO.Message;
+import com.aspire.OnlineMeal.service.IUserInfoService;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -32,6 +34,9 @@ public class MyWebSocketHandler implements WebSocketHandler {
 	//定义一个商家会话队列
 	public static final Map<String,WebSocketSession> marchantSocketSessionMap;
 	
+	@Autowired
+	private IUserInfoService iuis = null;
+	
 	static {
 		userSocketSessionMap = new HashMap<String,WebSocketSession>();
 		marchantSocketSessionMap = new HashMap<String,WebSocketSession>();
@@ -41,7 +46,14 @@ public class MyWebSocketHandler implements WebSocketHandler {
 	 */
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus arg1) throws Exception {
-		
+		String type = (String) session.getAttributes().get("type");
+		String id = (String) session.getAttributes().get("id");
+		if("user".equals(type)){
+			userSocketSessionMap.remove(id);
+		}else if("marchant".equals(type)){
+			marchantSocketSessionMap.remove(id);
+		}
+		System.out.println("sessionId: "+session.getId()+"已下线");
 	}
 
 	/**
@@ -49,15 +61,23 @@ public class MyWebSocketHandler implements WebSocketHandler {
 	 */
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		System.out.println(session.getUri());
 		System.out.println("--------连接成功------------");
 		System.out.println("webSocket id: "+session.getId());
+		
 		//区分是用户会话还是商家会话 然后将其存储起来
 		if("user".equals(session.getAttributes().get("type"))){
 			String userId = (String) session.getAttributes().get("id");
 			if(userSocketSessionMap.get(userId) == null){
 				userSocketSessionMap.put(userId, session);
 			}else{
+				//给旧session发送消息，已被迫使下线
+				WebSocketSession oldSession = userSocketSessionMap.get(userId);
+				
+				//此代码块有存在并发问题...
+				sendMessageToUserOutLogin(oldSession);
+				
+				oldSession.close();
+				System.out.println("已经迫使旧session下线");
 				userSocketSessionMap.put(userId, session);
 			}
 		}else if("marchant".equals(session.getAttributes().get("type"))){
@@ -67,17 +87,25 @@ public class MyWebSocketHandler implements WebSocketHandler {
 			if(marchantSocketSessionMap.get(marchantId) == null){
 				marchantSocketSessionMap.put(marchantId, session);
 			}else{
+				//给旧session发送消息，已被迫使下线
+				WebSocketSession oldSession = marchantSocketSessionMap.get(marchantId);
+				
+				//此代码块有存在并发问题...
+				sendMessageToUserOutLogin(oldSession);
+				
+				oldSession.close();
+				System.out.println("已经迫使旧session下线");
 				marchantSocketSessionMap.put(marchantId, session);
 			}
 		}
 		System.out.println("商家列表");
 		for(Map.Entry<String, WebSocketSession> entry : marchantSocketSessionMap.entrySet()){
-            System.out.println("键 key ："+entry.getKey()+" 值value ："+entry.getValue());
+            System.out.println("键 key: "+entry.getKey()+" 值value: "+entry.getValue()+"webscoketId: "+entry.getValue().getId());
         }
 		
 		System.out.println("用户列表");
 		for(Map.Entry<String, WebSocketSession> entry : userSocketSessionMap.entrySet()){
-            System.out.println("键 key ："+entry.getKey()+" 值value ："+entry.getValue());
+            System.out.println("键 key: "+entry.getKey()+" 值value: "+entry.getValue()+"webscoketId: "+entry.getValue().getId());
         }
 	}
 
@@ -109,10 +137,15 @@ public class MyWebSocketHandler implements WebSocketHandler {
 			session = userSocketSessionMap.get(id);
 		}
 		if(session != null && session.isOpen()){
-			System.out.println("发消息的session"+session.getId());
+			System.out.println("发消息的session："+session.getId());
 			session.sendMessage(message);
 		}
-		
+	}
+	
+	public void sendMessageToUserOutLogin(WebSocketSession session) throws Exception{
+		WebSocketMessage message = new TextMessage("code:10,message:尊敬的用户你的账号在"+new Date()+"在别处登陆,如非本人操作请更改密码");
+		System.out.println("给用户"+session.getId()+"发消息");
+		session.sendMessage(message);
 	}
 
 	@Override
