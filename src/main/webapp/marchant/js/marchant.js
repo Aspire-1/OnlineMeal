2282abd68b891df3eaf868f1b3a8d6aa6514ca70
Aspire-1 <873763823@qq.com>
2018-05-16 22:19:00 +0800

    商家页面

--- src/main/webapp/marchant/js/marchant.js
+++ src/main/webapp/marchant/js/marchant.js
@@ -8,9 +8,10 @@ layui.use('element', function(){
 	
 });
 var marchant;
-var cartData=[];
-var cartAllPrice = 0;
 $(function(){
+	window.sessionStorage.removeItem('cartData');
+	cartData=[];
+	cartAllPrice = 0;
 	$.ajax({
 		async: false,
 		type: 'POST',
@@ -23,7 +24,6 @@ $(function(){
 			marchant = data;
 		}
 	});
-	//$('#dishedInfoChoice').tmpl(value).appendTo(wordBlock);
 
 	$('#marchantBar').tmpl(marchant).appendTo(".marchant-bar");
 	console.log("marchantId"+window.sessionStorage.getItem("marchantId"));
@@ -43,7 +43,8 @@ $(function(){
 		}
 	});
 });
-
+var cartData=[];
+var cartAllPrice = 0;
 function clickChoiceBtn(){
 	
 	var inputElem = $(this).parent().prev("input").val();
@@ -57,21 +58,63 @@ function clickChoiceBtn(){
 	}else if($(this).attr("name")=="cart"){
 		var elem = $(this).closest(".dished-info-elem");
 		var record = {
-			
 			'dished_id':elem.find("#infoId").val(),
 			'dished_name':elem.find("#dishedName").html(),
 			'dished_count':elem.find("#count").val(),
 			'dished_price':elem.find("#price").val()
 		};
-		cartData.push(record);
-		console.log(cartData);
-		window.sessionStorage.setItem("cartData",JSON.stringify(cartData));
-		
-		$("#record").append("<tr><td>"+elem.find("#dishedName").html()+"</td><td>"+elem.find("#count").val()+"</td><td name='recordPrice'>￥"+elem.find("#price").val()*elem.find("#count").val()+"</td></tr>");
+		var exitCart = JSON.parse(window.sessionStorage.getItem("cartData"));
+		if(exitCart != null || exitCart!=undefined){
+			for(var x =0;x<exitCart.length;x++){
+				if(record.dished_id==exitCart[x].dished_id){
+					exitCart[x].dished_count = parseInt(exitCart[x].dished_count)+ parseInt(record.dished_count) +"";
+					//record.dished_count = exitCart[x].dished_count;
+					for(var i = 0;i<cartData.length;i++){
+						if(cartData[i].dished_id == record.dished_id){
+							cartData[i].dished_count = exitCart[x].dished_count;
+							break;
+						}
+					}
+					console.log("exitCart: ");
+					console.log(exitCart);
+					window.sessionStorage.setItem("cartData",JSON.stringify(exitCart));
+					console.log("cartData: ");
+					console.log(cartData);
+					break;
+				}else if(record.dished_id!=exitCart[x].dished_id && x == exitCart.length-1){
+					cartData.push(record);
+					console.log("cartData: ");
+					console.log(cartData);
+					window.sessionStorage.setItem("cartData",JSON.stringify(cartData));
+				}
+			}
+		}else{
+			cartData.push(record);
+			console.log("cartData: ");
+			console.log(cartData);
+			window.sessionStorage.setItem("cartData",JSON.stringify(cartData));
+		}
+		showCartData();
 		cartAllPrice= cartAllPrice + elem.find("#price").val()*elem.find("#count").val();
 		$("#allPrice").html(cartAllPrice);
+	
 	}
 	$(this).parent().prev("input").val(inputElem);
+}
+
+function showCartData(){
+	var showCartData = JSON.parse(window.sessionStorage.getItem("cartData"));
+	//console.log("showCartData: "+showCartData);
+	if(showCartData != null || showCartData!=undefined){
+		for(var i = 0;i<showCartData.length;i++){
+			if(i == 0){
+				$("#record").html('<tr><td colspan="3"><span><a id="clear">[清空]</a></span></td></tr>');
+				$("#record").append("<tr><td>"+showCartData[i].dished_name+"</td><td>"+showCartData[i].dished_count+"</td><td name='recordPrice'>￥"+showCartData[i].dished_count*showCartData[i].dished_price+"</td></tr>");
+			}else{
+				$("#record").append("<tr><td>"+showCartData[i].dished_name+"</td><td>"+showCartData[i].dished_count+"</td><td name='recordPrice'>￥"+showCartData[i].dished_count*showCartData[i].dished_price+"</td></tr>");
+			}
+		}
+	}
 }
 
 //动态加载元素的注册事件
@@ -96,7 +139,7 @@ function productDishedBox(value){
 	
 	var img = document.createElement("img");
 	img.setAttribute("alt",value.name);
-	img.setAttribute("src",value.photo);
+	img.setAttribute("src","./"+value.photoSrc);
 	
 	var wordBlock = document.createElement("div");
 	wordBlock.setAttribute("class","dished-info-word");
@@ -139,12 +182,18 @@ $(".shopCartArea .layui-btn-group [name=shopCart]").on("click",function(){
 
 $(".shopCartArea .layui-btn-group [name=placeOrder]").on("click",function(){
 	//结账
+	$("#contain").load("placeOrder/userOrder.html",function(){
+		
+	})
+	
 });
 
 
-$('#clear').on("click",function(event){
+$('.cartContain').on("click",'#clear',function(event){
 	$("#record").html('<tr><td colspan="3"><span><a id="clear">[清空]</a></span></td></tr>');
 	$("#allPrice").html(0);
+	window.sessionStorage.removeItem('cartData');
+	cartData=[];
 	cartAllPrice = 0;
 	event.preventDefault();
 });
