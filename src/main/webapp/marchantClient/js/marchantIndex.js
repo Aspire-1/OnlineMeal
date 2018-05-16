fbb2add1934b16c79c74bcc5833a03c9871dbe6e
Aspire-1 <873763823@qq.com>
2018-05-16 22:20:34 +0800

    商家端 商家端主页 商家端登陆页面 商家端菜肴信息管理 商家端订单管理

--- src/main/webapp/marchantClient/js/marchantIndex.js
+++ src/main/webapp/marchantClient/js/marchantIndex.js
@@ -1,4 +1,8 @@
+var marchantId=window.sessionStorage.getItem("marchantId");
 $(document).ready(function(){
+	if(marchantId==null|| marchantId==undefined){
+		window.location.href="loginMarchant.html";
+	}
 	$("div.layui-side div.layui-side-scroll li.layui-nav-item a").on("click",function(event){
 		var href=$(this).attr("href");
 		if(href!="#"){
@@ -9,4 +13,25 @@ $(document).ready(function(){
 		event.preventDefault();
 	});
 	
+	$("#logout").on("click",function logout(event){
+		
+		$.ajax({
+			async: false,
+			type:"POST",
+			url:"/OnlineMeal/MarchantManager/logout.mvc",
+			data:{
+				"id":window.sessionStorage.getItem("managerId")
+			},
+			success:function(data){
+				console.log(data)
+			}
+		});
+		window.sessionStorage.removeItem('marchantId');
+		window.sessionStorage.removeItem('managerId');
+
+		location.reload();
+		event.preventDefault();
+	});
+	
 });
+
